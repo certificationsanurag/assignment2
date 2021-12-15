b18d267a6479360e331519659e1cc7cb278968dd
certificationsanurag <96202740+certificationsanurag@users.noreply.github.com>
2021-12-16 01:23:07 +0530

    Create my-eks-cluster.tf

---
+++ my-eks-cluster.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.20"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t3a.large"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.my-eks-cluster_SG_2]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.my-eks-cluster_SG_1]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
