e607c1860d2ede1de08612bbe4c0cf1a59ad91e2
certificationsanurag <96202740+certificationsanurag@users.noreply.github.com>
2021-12-16 01:16:00 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "my-eks-cluster_SG" {
+resource "aws_security_group" "my-eks-cluster_SG_1" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
@@ -8,6 +8,19 @@ resource "aws_security_group" "my-eks-cluster_SG" {
 
     cidr_blocks = [
       "10.0.0.0/16",
+    ]
+  }
+}
+resource "aws_security_group" "my-eks-cluster_SG_2" {
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
     ]
   }
 }
